a5cd2e64dbbcfc51f3cc9e635e74fa8193657950
gyonnmi <107240999+gyonnmi@users.noreply.github.com>
2022-11-09 00:34:09 +0900

    feat: Cart Button Animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const cartCtx = useContext(CartContext);
 
+  // 객체 구조 분해 할당
+  const { items } = cartCtx;
+
   // curNumber = 현재 수량
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnAnimation ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      // 장바구니에 항목이 없다면 그냥 리턴
+      return;
+    }
+    setBtnAnimation(true);
+    // 애니메이션 클래스 제거
+    const timer = setTimeout(() => {
+      setBtnAnimation(false);
+    }, 300);
+
+    // 클린업 함수(타이머 제거)
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
